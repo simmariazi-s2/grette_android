c9a5f14a0e96cc45da46ec7da6a244d6fadd924e
Sydney <jinjoolee.0319@gmil.com>
2022-01-11 00:37:18 +0900

    회사명 확인 화면 연결

--- app/src/main/java/com/simmariazi/myapplication/ui/register/CompanyCheckFragment.kt
+++ app/src/main/java/com/simmariazi/myapplication/ui/register/CompanyCheckFragment.kt
@@ -1,18 +1,14 @@
 package com.simmariazi.myapplication.ui.register
 
-import android.app.ActionBar
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
 import com.simmariazi.myapplication.R
-import com.simmariazi.myapplication.databinding.FragmentEmailVerifyBinding
+import com.simmariazi.myapplication.databinding.FragmentCompanyCheckBinding
 import com.simmariazi.myapplication.databinding.FragmentPasswordBinding
-import com.simmariazi.myapplication.ui.LoginActivity
-import com.simmariazi.myapplication.ui.RegisterActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,14 +17,14 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PasswordFragment.newInstance] factory method to
+ * Use the [CompanyCheckFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PasswordFragment : Fragment() {
+class CompanyCheckFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private var binding: FragmentPasswordBinding? = null;
+    private var binding: FragmentCompanyCheckBinding? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +39,8 @@ class PasswordFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_password, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_company_check, container, false)
         return binding!!.root
     }
 
@@ -54,12 +51,12 @@ class PasswordFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PasswordFragment.
+         * @return A new instance of fragment CompanyCheckFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PasswordFragment().apply {
+            CompanyCheckFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
@@ -69,11 +66,10 @@ class PasswordFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding!!.btnOk.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(p0: View?) {
-                val controller = Navigation.findNavController(view)
-                controller.navigate(R.id.action_passwordFragment_to_companyCheckFragment)
-            }
-        })
+        return initialize()
+    }
+
+    private fun initialize() {
+
     }
 }
