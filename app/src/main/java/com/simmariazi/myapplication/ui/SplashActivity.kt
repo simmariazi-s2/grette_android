faf65eb17355f9d3e69029f60afc2cc1a45fdd21
USER_20211028 <baekhk1006@gmail.com>
2022-01-22 17:34:26 +0900

    1. 스플래쉬 액티비티 / 폰트 목록 추가

---
+++ app/src/main/java/com/simmariazi/myapplication/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.simmariazi.myapplication.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.simmariazi.myapplication.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
